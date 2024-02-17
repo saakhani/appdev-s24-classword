d94cb3e529ac8e92dcfe77c05d8f08ad2c166767
Saad Lakhani <muhammad.saad.002002@gmail.com>
2024-02-17 17:06:00 +0500

    Finish till step 6

--- lib/home.dart
+++ lib/home.dart
@@ -18,6 +18,37 @@ class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   // TODO: Make a collection of cards (102)
+  List<Card> _buildGridCards(int count){
+    List<Card> cards = List.generate(
+      count, 
+      (int index){
+        return Card(
+          clipBehavior: Clip.antiAlias,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              AspectRatio(
+                aspectRatio: 18.0 / 11.0,
+                child: Image.asset('assets/diamond.png'),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const <Widget>[
+                    Text('Title'),
+                    SizedBox(height: 8.0),
+                    Text('Secondary Text'),
+                  ]
+                )
+              )
+            ]
+          )
+        );
+      },
+    );
+    return cards;
+  }
   // TODO: Add a variable for Category (104)
   @override
   Widget build(BuildContext context) {
@@ -27,30 +58,34 @@ class HomePage extends StatelessWidget {
       // TODO: Add app bar (102)
       appBar: AppBar(
         leading: IconButton(
-          icon: const Icon(
-            Icons.menu,
-            semanticLabel: 'menu'),
-          onPressed: (){print("Menu Button")},
-          ),
+          icon: const Icon(Icons.menu, semanticLabel: 'menu'),
+          onPressed: () {
+            print("Menu Button");
+          },
+        ),
         title: const Text('SHRINE'),
         actions: <Widget>[
           IconButton(
-            icon: const Icon(
-              Icons.search,
-              semanticLabel: 'search'),
-            onPressed: (){print("Search Button")},
+            icon: const Icon(Icons.search, semanticLabel: 'search'),
+            onPressed: () {
+              print("Search Button");
+            },
           ),
           IconButton(
-            icon: const Icon(
-              Icons.tune,
-              semanticLabel: 'filter'),
-            onPressed: (){print("Filter Button")},
+            icon: const Icon(Icons.tune, semanticLabel: 'filter'),
+            onPressed: () {
+              print("Filter Button");
+            },
           )
         ],
       ),
       // TODO: Add a grid view (102)
-      body: Center(
-        child: Text('You did it!'),
+      body: GridView.count(
+        crossAxisCount: 2,
+        padding: const EdgeInsets.all(16.0),
+        childAspectRatio: 8.0 / 9.0,
+        // TODO: Build a grid of cards (102)
+        children: _buildGridCards(10)
       ),
       // TODO: Set resizeToAvoidBottomInset (101)
       resizeToAvoidBottomInset: false,
---
+++ lib/model/product.dart
@@ -0,0 +1,21 @@
+class Product {
+  final String imageUrl;
+  final String name;    
+  final String price;
+
+
+  Product({required this.imageUrl, required this.name, required this.price});
+}
+
+List<Product> _mockProducts = [
+  Product(imageUrl: "imageUrl", name: "name", price: "price"),
+  Product(imageUrl: "imageUrl", name: "name", price: "price"),    
+  Product(imageUrl: "imageUrl", name: "name", price: "price"), 
+  Product(imageUrl: "imageUrl", name: "name", price: "price"),
+  Product(imageUrl: "imageUrl", name: "name", price: "price"),    
+  Product(imageUrl: "imageUrl", name: "name", price: "price"), 
+  Product(imageUrl: "imageUrl", name: "name", price: "price"), 
+  Product(imageUrl: "imageUrl", name: "name", price: "price"), 
+  Product(imageUrl: "imageUrl", name: "name", price: "price"), 
+     
+]
