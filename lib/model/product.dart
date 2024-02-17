a8131ed9927ddcf404ba29860010ef018ac1c9d9
Saad Lakhani <muhammad.saad.002002@gmail.com>
2024-02-17 17:07:07 +0500

    Finish

--- lib/model/product.dart
+++ lib/model/product.dart
@@ -1,21 +1,21 @@
+import 'dart:ffi';
+
 class Product {
   final String imageUrl;
-  final String name;    
-  final String price;
-
+  final String name;
+  final double price;
 
   Product({required this.imageUrl, required this.name, required this.price});
 }
 
-List<Product> _mockProducts = [
-  Product(imageUrl: "imageUrl", name: "name", price: "price"),
-  Product(imageUrl: "imageUrl", name: "name", price: "price"),    
-  Product(imageUrl: "imageUrl", name: "name", price: "price"), 
-  Product(imageUrl: "imageUrl", name: "name", price: "price"),
-  Product(imageUrl: "imageUrl", name: "name", price: "price"),    
-  Product(imageUrl: "imageUrl", name: "name", price: "price"), 
-  Product(imageUrl: "imageUrl", name: "name", price: "price"), 
-  Product(imageUrl: "imageUrl", name: "name", price: "price"), 
-  Product(imageUrl: "imageUrl", name: "name", price: "price"), 
-     
-]
+List<Product> mockProducts = [
+  Product(imageUrl: "imageUrl", name: "Vagabond", price: 120.00),
+  Product(imageUrl: "imageUrl", name: "name", price: 0.00),
+  Product(imageUrl: "imageUrl", name: "name", price: 0.00),
+  Product(imageUrl: "imageUrl", name: "name", price: 0.00),
+  Product(imageUrl: "imageUrl", name: "name", price: 0.00),
+  Product(imageUrl: "imageUrl", name: "name", price: 0.00),
+  Product(imageUrl: "imageUrl", name: "name", price: 0.00),
+  Product(imageUrl: "imageUrl", name: "name", price: 0.00),
+  Product(imageUrl: "imageUrl", name: "name", price: 0.00),
+];
