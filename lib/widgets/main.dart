bf9b97821c57ca8a31c19d6e63f9cce90f3b3138
Saad Lakhani <muhammad.saad.002002@gmail.com>
2024-02-24 16:16:31 +0500

    Add stuff or wtv

--- lib/widgets/main.dart
+++ lib/widgets/main.dart
@@ -1,7 +1,9 @@
-import 'package:flutter/material.dart';
-import 'package:./lib/models/albums_model.dart';
+import 'dart:convert';
 
+import 'package:flutter/material.dart';
+import '../models/album_model.dart';
 import 'package:http/http.dart' as http;
+
 
 void main() {
   runApp(const MyApp());
@@ -20,50 +22,101 @@ class MyApp extends StatelessWidget {
         useMaterial3: false,
       ),
       darkTheme: ThemeData.dark(),
-      home: const Login(title: 'Login'),
+      home: const UsersScreen(title: 'Login'),
     );
   }
 }
 
-class Login extends StatefulWidget {
-  const Login({super.key, required this.title});
+class UsersScreen extends StatefulWidget {
+  const UsersScreen({super.key, required this.title});
 
   final String title;
 
   @override
-  State<Login> createState() => UserScreenState();
+  State<UsersScreen> createState() => UserScreenState();
 }
 
 //list view builder only renders those that are being displayed on screen
 // thats why its preferred to normals rows and columns
 
-class UserScreenState extends State<Login> {
+class UserScreenState extends State<UsersScreen> {
+  //late will throw error when used before assigning
+  late Future<List<Albums>> futureAlbumsList;
+
   Future<List<Albums>> fetchAlbums() async {
     Uri uriObject = Uri.parse('https://jsonplaceholder.typicode.com/albums');
     final response = await http.get(uriObject);
-    
-    if (response.statusCode == 200){
 
+    if (response.statusCode == 200) {
+      List<dynamic> parseListJson = jsonDecode(response.body);
+
+      //cant access by iterable through index
+      List<Albums> items = List<Albums>.from(
+        //map returns and interable
+        parseListJson.map<Albums>((dynamic user) => Albums.fromJson(user)),
+      );
+
+      //.from is more optimized than .tolist()
+
+      return items;
+    } else {
+      throw Exception('Failed to load album');
     }
+  }
+
+  //load the widget before do this stuff
+  @override
+  void initState() {
+    super.initState();
+
+    futureAlbumsList = fetchAlbums();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: ListView.builder(
-              itemCount: 5,
-              itemBuilder: (context, index) {
-                return Card(
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      backgroundImage: AssetImage("assets/catpic.jpeg"),
+          child: FutureBuilder(
+        future: futureAlbumsList,
+        //snapshot is the curret state of the data
+
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+                itemCount: snapshot.data?.length,
+                itemBuilder: (context, index) {
+                  var user = snapshot.data![index];
+                  return Card(
+                    child: ListTile(
+                      leading: const CircleAvatar(
+                        backgroundImage: NetworkImage('https://images.pexels.com/photos/9248672/pexels-photo-9248672.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
+                      ),
+                      title: Text(user.title),
+                      subtitle: Text(index.toString()),
+                      // onTap: () {
+                      //   showMaterialModalBottomSheet(
+                      //     context: context,
+                      //     builder: (context) => Container(
+                      //       child: Column(
+                      //         children: [
+                      //           Text(user.title),
+                      //           Text(user.id.toString()),
+                      //         ],
+                      //       ),
+                      //     ),
+                      //   );
+                      // },
                     ),
-                    title: Text(index.toString()),
-                    subtitle: const Text("hello"),
-                  ),
-                );
-              })),
+                  );
+                });
+          }
+          if (snapshot.hasError) {
+            return Text(snapshot.error.toString());
+          }
+
+          return const CircularProgressIndicator();
+        },
+      )),
     );
   }
 }
