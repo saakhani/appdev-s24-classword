075b832a8f6a32aeb9926cc551fd650d1638f43d
saakhani <muhammad.saad.002002@gmail.com>
2024-04-14 16:07:45 +0500

    Refactor main.dart and home_page.dart

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -13,9 +13,9 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
-    // WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-    //   Provider.of<TodoProvider>(context, listen: false).getAllTodos();
-    // });
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      Provider.of<TodoProvider>(context, listen: false).getAllTodos();
+    });
   }
 
   @override
@@ -23,27 +23,27 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
         appBar: AppBar(
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text("Provider"),
+          title: const Text("Provider"),
         ),
-        // body: Consumer<TodoProvider>(builder: (context, value, child) {
-        //   if (value.isLoading) {
-        //     return const Center(child: CircularProgressIndicator());
-        //   }
+        body: Consumer<TodoProvider>(builder: (context, value, child) {
+          if (value.isLoading) {
+            return const Center(child: CircularProgressIndicator());
+          }
 
-        //   final todos = value.todos;
-        //   return ListView.builder(
-        //       itemCount: todos.length,
-        //       itemBuilder: (context, index) {
-        //         return ListTile(
-        //           leading: CircleAvatar(
-        //             child: Text((index + 1).toString()),
-        //           ),
-        //           title: Text(todos[index].title),
-        //           trailing:
-        //               todos[index].completed ? const Icon(Icons.done) : null,
-        //         );
-        //       });
-        // })
+          final todos = value.todos;
+          return ListView.builder(
+              itemCount: todos.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: CircleAvatar(
+                    child: Text((index + 1).toString()),
+                  ),
+                  title: Text(todos[index].title),
+                  trailing:
+                      todos[index].completed ? const Icon(Icons.done) : null,
+                );
+              });
+        })
         );
   }
 }
